223242cc88f4a4ff8a9d82966683e848109db047
Abdou <abdousall267@gmail.com>
2024-04-01 23:05:26 +0200

    Initial commit

---
+++ src/main/java/com/projetmemoire/optimiserlotissement/OptimiserlotissementApplication.java
@@ -0,0 +1,13 @@
+package com.projetmemoire.optimiserlotissement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimiserlotissementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimiserlotissementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetmemoire/optimiserlotissement/OptimiserlotissementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetmemoire.optimiserlotissement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimiserlotissementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
